485c98e0c70fe300f099865431553481c1b7d7d5
ze-fernando <jfenginner935@gmail.com>
2023-10-25 22:13:35 -0300

    Create client and finish server

--- server/Program.cs
+++ server/Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.AspNetCore.SignalR;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapHub<MyHub>("/chat");
 
 app.Run();
+
+class MyHub : Hub
+{
+    public async IAsyncEnumerable<DateTime> Streaming(CancellationToken cct)
+    {
+        yield return DateTime.Now;
+        await Task.Delay(1000, cct);
+    }
+}
