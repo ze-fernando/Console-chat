ec5d680b93e6a60585fcd5e9da54ac412c405486
ze-fernando <jfenginner935@gmail.com>
2023-10-25 22:26:53 -0300

    Fix bug and config client

--- client/Program.cs
+++ client/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+const string url = "http://localhost:5091/chat";
+
+await using var connection = new HubConnectionBuilder()
+   .WithUrl(url)
+    .Build();
+
+await connection.StartAsync();
+
+await foreach (var date in connection.
+    StreamAsync<DateTime>("Streaming"))
+{
+    Console.WriteLine(date);
+}
